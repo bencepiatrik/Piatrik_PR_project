84c9154ea44b29de094e418665103c83a463a2a4
bencepiatrik <bence.piatrik@azet.sk>
2023-12-18 12:57:41 +0100

    css update

--- parts/editForm.php
+++ parts/editForm.php
@@ -102,42 +102,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-<div id="editFormContainer" class="edit-form-container">
-    <form method="post" action="./parts/editForm.php">
-        <h2>Edit Product</h2>
-        <div class="mb-3">
-            <label for="editedProductName" class="form-label">Name:</label>
-            <input type="text" name="editedProductName" value="<?php echo $productToEdit['name']; ?>" required class="form-control">
-        </div>
-        <div class="mb-3">
-            <label for="editedProductPrice" class="form-label">Price:</label>
-            <input type="number" name="editedProductPrice" value="<?php echo $productToEdit['price']; ?>" step="0.01" required class="form-control">
-        </div>
-        <div class="mb-3">
-            <label for="editedProductProperties" class="form-label">Properties:</label>
-            <input type="text" name="editedProductProperties" value="<?php echo $productToEdit['properties']; ?>" class="form-control">
-        </div>
-        <div class="mb-3">
-            <label for="editedProductStars" class="form-label">Stars:</label>
-            <input type="number" name="editedProductStars" value="<?php echo $productToEdit['stars']; ?>" class="form-control">
-        </div>
-        <div class="mb-3">
-            <label for="editedProductImgSrc" class="form-label">Image Source:</label>
-            <input type="text" name="editedProductImgSrc" value="<?php echo $productToEdit['img_src']; ?>" class="form-control">
-        </div>
-        <div class="form-check mb-3">
-            <input type="checkbox" name="editedProductFeatured" <?php echo $productToEdit['featured']; ?> class="form-check-input">
-            <label for="editedProductFeatured" class="form-check-label">Featured</label>
-        </div>
-        <div class="form-check mb-3">
-            <input type="checkbox" name="editedProductFlashDeal" <?php echo $productToEdit['flash_deal']; ?> class="form-check-input">
-            <label for="editedProductFlashDeal" class="form-check-label">Flash Deal</label>
-        </div>
-        <div class="form-check mb-3">
-            <input type="checkbox" name="editedProductLastMinute" <?php echo $productToEdit['last_minute']; ?> class="form-check-input">
-            <label for="editedProductLastMinute" class="form-check-label">Last Minute</label>
-        </div>
-        <input type="hidden" name="productId" value="<?php echo $productToEdit['id']; ?>">
-        <button type="submit" name="saveEdit" class="btn btn-primary">Save Changes</button>
-    </form>
+<div class="container" style="margin-top: 5rem">
+    <div id="editFormContainer" class="edit-form-container">
+        <form method="post" action="./parts/editForm.php">
+            <h2>Edit Product</h2>
+            <div class="mb-3">
+                <label for="editedProductName" class="form-label">Name:</label>
+                <input type="text" name="editedProductName" value="<?php echo $productToEdit['name']; ?>" required class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="editedProductPrice" class="form-label">Price:</label>
+                <input type="number" name="editedProductPrice" value="<?php echo $productToEdit['price']; ?>" step="0.01" required class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="editedProductProperties" class="form-label">Properties:</label>
+                <input type="text" name="editedProductProperties" value="<?php echo $productToEdit['properties']; ?>" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="editedProductStars" class="form-label">Stars:</label>
+                <input type="number" name="editedProductStars" value="<?php echo $productToEdit['stars']; ?>" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="editedProductImgSrc" class="form-label">Image Source:</label>
+                <input type="text" name="editedProductImgSrc" value="<?php echo $productToEdit['img_src']; ?>" class="form-control">
+            </div>
+            <div class="form-check mb-3">
+                <input type="checkbox" name="editedProductFeatured" <?php echo $productToEdit['featured']; ?> class="form-check-input">
+                <label for="editedProductFeatured" class="form-check-label">Featured</label>
+            </div>
+            <div class="form-check mb-3">
+                <input type="checkbox" name="editedProductFlashDeal" <?php echo $productToEdit['flash_deal']; ?> class="form-check-input">
+                <label for="editedProductFlashDeal" class="form-check-label">Flash Deal</label>
+            </div>
+            <div class="form-check mb-3">
+                <input type="checkbox" name="editedProductLastMinute" <?php echo $productToEdit['last_minute']; ?> class="form-check-input">
+                <label for="editedProductLastMinute" class="form-check-label">Last Minute</label>
+            </div>
+            <input type="hidden" name="productId" value="<?php echo $productToEdit['id']; ?>">
+            <button type="submit" name="saveEdit" class="btn btn-primary">Save Changes</button>
+        </form>
+    </div>
 </div>
